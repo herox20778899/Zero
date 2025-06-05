0f290c2df12bf3366eb76e1bb5bbfb112a96e627
herox20778899 <87426187+herox20778899@users.noreply.github.com>
2025-06-05 12:06:00 +0100

    Update routes.ts

--- apps/mail/app/routes.ts
+++ apps/mail/app/routes.ts
@@ -27,9 +27,9 @@ export default [
 
   route('/login', '(auth)/login/page.tsx'),
 
-  // Enable this when we have a zero signup page
-  // route('/zero/signup', '(auth)/zero/signup/page.tsx'),
-  // route('/zero/login', '(auth)/zero/login/page.tsx'),
+  
+  route('/zero/signup', '(auth)/zero/signup/page.tsx'),
+  route('/zero/login', '(auth)/zero/login/page.tsx'),
 
   layout('(routes)/layout.tsx', [
     route('/developer', '(routes)/developer/page.tsx'),
